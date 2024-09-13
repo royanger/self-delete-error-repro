a92d8f4c76a1ca2a9c9430c8d29e76d1bbbaec4b
Roy Anger <roy@royanger.com>
2024-09-13 18:10:36 -0400

    Updated app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import { Button } from "@/components/ui/button";
 import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <SignInButton>
-        <Button>Sign Out</Button>
+        <Button>Sign In</Button>
       </SignInButton>
 
       <SignUpButton>
         <Button>Sign Out</Button>
       </SignUpButton>
+
       <UserButton />
+
+      <Link href="/private">Protected Page</Link>
     </>
   );
 }
--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -2,13 +2,14 @@ import { UserButton } from "@clerk/nextjs";
 import { auth, currentUser } from "@clerk/nextjs/server";
 
 export default async function Private() {
-  // const user = await currentUser();
-  const { userId } = auth()
+  const user = await currentUser();
+  // const { userId } = auth()
   return (
     <>
+      <p>Protected page</p>
       <UserButton />
-      {/* {user.id} */}
-      {userId}
+      {user?.id}
+      {/* {userId} */}
     </>
   );
 }
